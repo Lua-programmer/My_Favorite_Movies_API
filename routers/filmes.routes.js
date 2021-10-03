d8133d19dc2c6753ce7d91af84a862af773a59b7
Lua-programmer <[luanamelissaprogrammer@gmail.com]>
2021-10-03 00:30:09 -0300

    PUT E DELETE e algumas validações

--- routers/filmes.routes.js
+++ routers/filmes.routes.js
@@ -1,6 +1,11 @@
 const express = require('express'); //IMPORTANDO BIBLIOTECA EXPRESS
 const router = express.Router(); //RETORNANDO UM ROTADOR QUE NOS PERMITE CONFIGURAR TODAS AS ROTAS DA NOSSA APLICAÇÃO
 
+//CONSTANTES PARA RESPOSTAS POSITIVAS DOS MÉTODOS
+const atualizado = "Parabéns, você conseguiu atualizar o filme com sucesso, retorne a página principal para conferir a atualização!";
+const excluido = "Você excluiu o filme, retorne a página principal para checar a exclusão";
+
+
 const Filmes = [
     {
         nome: 'Capitã Marvel',
@@ -12,9 +17,29 @@ const Filmes = [
 ] 
 //O MÉTODO Date.now() RETORNA O NÚMERO DE MILISEGUNDOS DECORRIDOS DESDE 1° DE JANEIRO DE 1970 00:00:00 UTC. DESSA FORMA UTILIZANDO ESSE MÉTODO NO ID DO PROJETO IREMOS TER SEMPRE UM 'ID' DIFERENTE.
 
+
+
+//CONSTANTES DE VALIDAÇÃO
+
+const getFilmesValidos = () => Filmes.filter(Boolean);
+
+const getFilmeIndexById = (id) =>
+  getFilmesValidos().findIndex((filme) => filme.id == id);
+
+
+//-----------------------------INICIO DO CRUD----------------------------------------------------------//
+
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------//
 //GET /filmes - RETORNANDO UMA LISTA DE FILMES
 router.get('/', (req, res) => {
+    
+    //VALIDAÇÃO
+    //CASO O ARRAY ESTEJA VAZIO IRÁ RETORNAR A MENSAGEM ABAIXO
+    if (Filmes.length == 0) {
+        res.send("A página não possui filmes cadastrados");
+        return;
+    }
+
     res.send(Filmes);
 })
 
@@ -40,26 +65,73 @@ router.get('/:id', (req, res) => {
 })
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------//
-//POST - /add  - CADASTRAR UM NOVO FILME
+//POST - /filmes/add  - CADASTRAR UM NOVO FILME
 router.post('/add', (req, res) => {
     const filme = req.body;
     filme.id = Date.now()
 
     //VALIDAÇÃO
+    //PARA CADASTRAR UM NOVO FILME O BODY PRECISA ESTAR TOTALMENTE PREENCHIDO, SE NÃO ESTIVER IRÁ RETORNAR UMA MENSAGEM DE PEDIDO RUIM 
     if (!filme || !filme.nome || !filme.genero || !filme.nota || !filme.urlImagem) {
         console.log(400);
         res.status(400).send("Bad request");
     }else{
-        
+        //CASO CONTRÁRIO, O FILME SERÁ ADICIONADO AO ARRAY
         Filmes.push(filme);
     }
-
+    //RETORNANDO UMA MENSAGEM COM O FILME ADICIONADO 
+    console.log(201);
     res.status(201).send(filme);
 })
 
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------//
+//PUT - /filmes/id  - ATUALIZANDO FILME POR ID
+router.put('/:id', (req, res) => {
+    const novoFilme = req.body;
+    const id = req.params.id;
+    let filmeExistente = Filmes.find((filme) => filme.id == id);
 
+    filmeExistente.nome = novoFilme.nome;
+    filmeExistente.genero = novoFilme.genero;
+    filmeExistente.nota = novoFilme.nota;
+    filmeExistente.urlImagem = novoFilme.urlImagem;
 
+    //VALIDAÇÃO
+    //SE NÃO TIVER NENHUM FILME CADASTRADO NO ARRAY, IRÁ MOSTRAR UM ERRO 400
+    if (!Object.keys(novoFilme).length) {
+        console.log(400);
+        res.status(400).send("Bad request");
+        return;
+    }
 
+    //SE O BODY NÃO ESTIVER PREENCHIDO IRÁ RETORNAR UM ERRO 400
+    if (!novoFilme || !novoFilme.nome || !novoFilme.genero || !novoFilme.nota || !novoFilme.urlImagem) {
+        console.log(400);
+        res.status(400).send("Bad request");
+        return;
+    }
 
+    res.send(atualizado);
+
+})
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------//
+//DELETE  - /filmes/id  - EXCLUINDO FILME POR ID
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const filmeIndex = getFilmeIndexById(id);
+
+    //VALIDAÇÃO
+    //SE O ID DO FILME FOR MENR QUE ZERO OU DIFERENTE DE ALGUM ID JÁ EXISTENTE RETORNARÁ ERRO 404
+    if (filmeIndex < 0) {
+        res.status(404).send("Sorry can't find that!");
+        return;
+    }
+
+    Filmes.splice(filmeIndex, 1);
+
+    res.send(excluido);
+    
+})
 
 module.exports = router; // EXPORTANDO O MÓDULO QUE RETORNA O OBJETO ROUTER
